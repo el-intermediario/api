9d9eb0a8c4defe4f7560fa5c1d77e4305830ba79
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-13 11:16:45 -0300

    add file article

---
+++ src/apiServices/Articles/article.js
@@ -0,0 +1,56 @@
+//File path caller
+const mongoose = require('require'); 
+const {Schema} = mongoose; 
+
+//We define scheme for article
+const articleSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: 'User'
+    },
+    firstName: {
+        type: String,
+        default: null
+    }, 
+    lastName: {
+        type: String,
+        default: null
+    },
+    phone: {
+        type: Number, 
+        default: null
+    },
+    phoneArea: {
+        type: Number, 
+        default: null
+    },
+    typeId: {
+        type: String,
+        default: null
+    },
+    numberId: {
+        type: Number,
+        default: null
+    },
+    gender: {
+        type: String, 
+        default: null 
+    }, 
+    birthday: {
+        type: String,
+        default: null
+    }, 
+    imagen: {
+        type: String,
+        default: null
+    },
+    status: {
+        type: String,
+        default: null
+    }
+})
+
+const Article = mongoose.model('Article', articleSchema);
+module.exports = Article; 
+
+
