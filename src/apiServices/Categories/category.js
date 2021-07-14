72361c84773dbf6c88c091ea6627c936e37d0b00
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-14 11:59:17 -0300

    I added a categories folder

---
+++ src/apiServices/Categories/category.js
@@ -0,0 +1,50 @@
+//file path caller
+const mongoose = require("require");
+const { Schema } = mongoose;
+
+//We define scheme for category
+const categorySchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Type.ObjectId,
+        ref: "User",
+    },
+    title: {
+        type: String,
+        default: null,
+    },
+    body: {
+        type: String,
+        default: null,
+    },
+    date: {
+        type: Number,
+        default: null,
+    },
+    fieldName: {
+        type: String,
+        default: null,
+    },
+    fieldColor: {
+        type: String,
+        default: null,
+    },
+    fountain: {
+        type: String,
+        default: null,
+    },
+    flywhell: {
+        type: String,
+        default: null,
+    },
+    image: {
+        type: String,
+        default: null,
+    },
+    status: {
+        type: Boolean,
+        default: null,
+    },
+});
+
+const Categories = mongoose.model("Categories", categorySchema);
+module.exports = Categories;
