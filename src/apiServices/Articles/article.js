8c930223a7f5bdb30578fc67375bd4e7d34e6d95
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-14 12:03:09 -0300

    new fields were added to Article

--- src/apiServices/Articles/article.js
+++ src/apiServices/Articles/article.js
@@ -1,36 +1,42 @@
 //File path caller
-const mongoose = require('require'); 
-const {Schema} = mongoose; 
+const mongoose = require("require");
+const { Schema } = mongoose;
 
 //We define scheme for article
 const articleSchema = new Schema({
-    userId: {
-        type: mongoose.Schema.Type.ObjectId,
-        ref: 'User'
-    },
-    title: {
-        type: String,
-        default: null
-    }, 
-    body: {
-        type: String,
-        default: null
-    },
-    date: {
-        type: Number, 
-        default: null
-    }, 
-    image: {
-        type: String,
-        default: null
-    },
-    status: {
-        type: Boolean,
-        default: null
-    }
-})
+  userId: {
+    type: mongoose.Schema.Type.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    default: null,
+  },
+  body: {
+    type: String,
+    default: null,
+  },
+  date: {
+    type: Number,
+    default: null,
+  },
+  fountain: {
+    type: String,
+    default: null,
+  },
+  flywheel: {
+    type: String,
+    default: null,
+  },
+  image: {
+    type: String,
+    default: null,
+  },
+  status: {
+    type: Boolean,
+    default: null,
+  },
+});
 
-const Articles = mongoose.model('Articles', articleSchema);
-module.exports = Articles; 
-
-
+const Articles = mongoose.model("Articles", articleSchema);
+module.exports = Articles;
