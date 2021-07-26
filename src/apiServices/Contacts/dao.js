bf625457dc8d21af415ce185def14f54202e1f77
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-26 11:59:23 -0300

    dao file was added to Contacts folder

---
+++ src/apiServices/Contacts/dao.js
@@ -0,0 +1,17 @@
+const Contact = require('../contact.js');
+
+module.exports = { 
+    async post(Contact) { 
+        return new Promise((resolve, reject) => Contact.create(Contact, (err, docs) => {
+            if (err) return reject(err); 
+            return resolve(docs);
+        }));
+    }, 
+
+    async get(path) {
+        return new Promise((resolve, reject) => Contact.filOne({slug: path}, (err, docs) => {
+            if (err) return reject(err);
+            return resolve(docs);
+        }));
+    },
+}
