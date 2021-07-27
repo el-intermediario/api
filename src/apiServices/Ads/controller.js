1a596dd9c0b741f48ea2a2db5e0b7e8164cdde1f
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-27 11:42:33 -0300

    controller file was added to Ads folder

---
+++ src/apiServices/Ads/controller.js
@@ -0,0 +1,30 @@
+const redis = require('redis'); 
+const clientRedis = redis.createClient({ host: 'redis'});
+const dto = require('./dto');
+const action = require('./actions');
+
+async function post(req, res) {
+    const ad = await action.post(req.body);
+    return res.send(dto.single(ad));
+};
+
+async function get(req, res) {
+    const ad = await ad.get(req.params.path);
+    return res.send(dto.single(ad));
+};
+
+async function getAds(req, res) {
+    const ad = await  clientRedis.get('ad', async (err, data) => {
+        if (err) throw err; 
+        if (data) {
+            res.status(200).send(JSON.parse(data));
+        } else {
+        }
+    });
+}
+
+module.exports = {
+    get,
+    post,
+    getAds
+}
