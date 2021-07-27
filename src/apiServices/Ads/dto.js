0b7d22c59abf282d3d5c422747ea6593232c88d2
AxelCastr00 <axelfabcastro27@gmail.com>
2021-07-27 11:45:34 -0300

    dto file was added to Ads folder

---
+++ src/apiServices/Ads/dto.js
@@ -0,0 +1,10 @@
+const single = (resource) => ({
+    name: resource.name
+});
+
+const multiple = (resource) => resource.map(resource => single(resource));
+
+module.exports = {
+    single,
+    multiple
+}
