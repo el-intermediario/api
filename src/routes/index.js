ee919470c6c50c1e013ce2359cb9f7224452d455
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-02 17:41:20 -0300

    routings were added

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,20 @@ const uploadController = require("../apiServices/Upload/controller");
 // Routes.
 const authRouter = require('../apiServices/Auth/routes');
 const articlesRouter = require('../apiServices/Articles/routes');
+const categoriesRouter = require('../apiServices/Categories/routes');
+const contactsRouter = require('../apiServices/Contacts/routes');
+const tagsRouter = require('../apiServices/Tags/routes');
+const adsRouter = require('../apiServices/Ads/routes');
 const emailRouter = require('../services/SendEmail');
 const usersRouter = require('../apiServices/Users/routes');
 
 const router = express.Router();
 router.use('/auth', authRouter);
+router.use('/ads', adsRouter);
 router.use('/articles', articlesRouter);
+router.use('/categories', categoriesRouter);
+router.use('/contacts', contactsRouter);
+router.use('/tags', tagsRouter);
 router.use('/send-email', emailRouter);
 router.use('/users', usersRouter);
 
