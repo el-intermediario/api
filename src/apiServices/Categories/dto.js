ce2bd8ba290f3562955009e276af408b304b179b
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-02 17:38:19 -0300

    changes were added

--- src/apiServices/Categories/dto.js
+++ src/apiServices/Categories/dto.js
@@ -1,6 +1,14 @@
 const single = (resource) => ({
     name: resource.name,
     color: resource.color,
+    politics: resource.politics,
+    sport: resource.sport,
+    economy: resource.economy,
+    police: resource.police,
+    society: resource.society,
+    national: resource.national,
+    province: resource.province,
+    world: resource.world, 
 });
 
 const multiple = (resource) => resource.map((resource) => single(resource));
