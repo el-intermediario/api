f285a8be57a3f2ae91683e51297ce1ffc81e0364
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-02 17:36:18 -0300

    controller added to backend

--- src/apiServices/Categories/controller.js
+++ src/apiServices/Categories/controller.js
@@ -2,6 +2,7 @@ const redis = require('redis');
 const clientRedis = redis.createClient({ host: 'redis'});
 const dto = require('./dto');
 const action = require('./actions');
+const doa = require('./dao');
 
 async function post(req, res) {
     const category = await action.post(req.body);
@@ -14,7 +15,7 @@ async function get(req, req) {
 };
 
 async function getCategories(req, res) {
-    const category = await clientRedis.get('categoty', async (err, data) => {
+    const category = await clientRedis.get('category', async (err, data) => {
         if (err) throw err;
         if (data) {
             res.status(200).send(JSON.parse(data));
