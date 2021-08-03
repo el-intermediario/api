93ddf8ee5b7b03550e8c6a8e65db76fb360a3339
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-02 22:01:10 -0300

    the file was fixed

--- src/apiServices/Categories/category.js
+++ src/apiServices/Categories/category.js
@@ -8,43 +8,6 @@ const categorySchema = new Schema({
     type: String,
     default: null,
   },
-  color: {
-    type: String,
-    default: null,
-  },
-  politics: { 
-    type: String,
-    default: null,
-  },
-  sport: {
-    type: String,
-    default: null,
-  },
-  economy: {
-    type: String, 
-    default: null,
-  },
-  police: {
-    type: String, 
-    default: null, 
-  },
-  society: {
-    type: String,
-    default: null,
-  },
-  national: {
-    type: String,
-    default: null,
-  },
-  province: {
-    type: String, 
-    default: null,
-  },
-  world: {
-    type: String,
-    default: null, 
-  },
-
 });
 
 const Category = mongoose.model('Category', categorySchema);
