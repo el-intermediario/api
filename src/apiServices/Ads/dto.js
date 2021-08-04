0cf4e8bb662f7c5ce2e3ba38c5c19c989ffe6ca1
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-04 13:33:51 -0300

    new resources were added to the file dto

--- src/apiServices/Ads/dto.js
+++ src/apiServices/Ads/dto.js
@@ -1,6 +1,9 @@
 const single = (resource) => ({
     name: resource.name,
-    color: resource.color
+    color: resource.color,
+    type: resource.type,
+    image: resource.image, 
+    status: resource.status,
 });
 
 const multiple = (resources) => resource.map(resource => single(resource));
