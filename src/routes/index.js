51997213e05caf95b2b005667d34a78cef00340a
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-18 01:04:14 -0300

    new routes added in index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,6 +11,7 @@ const tagsRouter = require('../apiServices/Tags/routes');
 const adsRouter = require('../apiServices/Ads/routes');
 const emailRouter = require('../services/SendEmail');
 const usersRouter = require('../apiServices/Users/routes');
+const orderRouter = require('../apiServices/Orders/routes');
 
 const router = express.Router();
 router.use('/auth', authRouter);
@@ -21,6 +22,7 @@ router.use('/contacts', contactsRouter);
 router.use('/tags', tagsRouter);
 router.use('/send-email', emailRouter);
 router.use('/users', usersRouter);
+router.use('/orders', orderRouter);
 
 router.post("/upload", upload, uploadController.uploadFile);
 
