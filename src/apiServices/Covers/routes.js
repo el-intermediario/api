b63a0b24e4a031d140240767e483e8c25bfceeb1
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-19 20:30:42 -0300

    The error of the routes file in the covers folder has been solved

--- src/apiServices/Covers/routes.js
+++ src/apiServices/Covers/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('./controller');
 
 router.get('/', controller.getCover);
-router.get('/:path', cover.get);
+router.get('/:path', controller.get);
 router.get('/', controller.post);
 
 module.exports = router;
