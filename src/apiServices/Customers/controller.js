b182f7479c4a8fa6467b3a1eb37bd2558557893c
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-21 10:09:26 -0300

    the controller file was added to the customers folder

---
+++ src/apiServices/Customers/controller.js
@@ -0,0 +1,30 @@
+const redis = require('redis');
+const clienteRedis = redis.createClient({host: 'redis'});
+const dto = require('./dto');
+const action = require('./actions');
+
+async function post(req, res) { 
+    const customer = await action.post(req.body);
+    return res.send(dto.single(customer));
+};
+
+async function get(req, res) {
+    const customer = await action.get(req.params.path);
+    return res.send(dto.single(customer));
+};
+
+async function getCustomer(req, res) {
+    const customer = await clienteRedis.get('customer', async (err, data) => {
+        if (err) throw err;
+        if (data) {
+            res.status(200).send(JSON.parse(data));
+        }else {
+        }
+    });
+}
+
+module.exports = { 
+    get,
+    post,
+    getCustomer
+}
