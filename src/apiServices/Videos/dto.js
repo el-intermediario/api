c16836da4d395fe059722a36e310dd282840ce16
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-21 20:17:22 -0300

    the resource error in the Videos folder was fixed

--- src/apiServices/Videos/dto.js
+++ src/apiServices/Videos/dto.js
@@ -6,7 +6,7 @@ const single = (resource) => ({
     
 });
 
-const multiple = (resources) => resource.map((resource) => single(resource));
+const multiple = (resources) => resources.map((resource) => single(resource));
 
 module.exports = {
     single,
