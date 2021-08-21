82dc3c90bbc7ba0bbd272fffe2ec2131d1cfd62e
AxelCastr00 <axelfabcastro27@gmail.com>
2021-08-21 09:56:05 -0300

    the controller file was added to the cards folder

---
+++ src/apiServices/Cards/controller.js
@@ -0,0 +1,31 @@
+const redis = require('redis');
+const clienteRedis = redis.createClient({host: 'redis'}); 
+const dto = require('./dto'); 
+const action = require('./actions');
+const card = require('./card');
+
+async function post(req, res) {
+    const card = await action.post(req.body);
+    return res.send(dto.single(card));
+};
+
+async function get(req, res) {
+    const customer = await action.get(req.params.path);
+    return res.send(dto.single(card));
+};
+
+async function getCard(req, res) {
+    const card = await clienteRedis.get('card', async(err, data) => {
+        if(err) throw err;
+        if(data) {
+            res.status(200).send(JSON.parse(data));
+        }else {
+        }
+    });
+}
+
+module.exports = {
+    get,
+    post, 
+    getCard
+}
