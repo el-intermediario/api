04aa994a637151b88640978c52f5c198a998873d
Matias Torres <matiastorres.dev@gmail.com>
2023-01-01 14:45:16 -0300

    Test cache in categories.

--- src/apiServices/Categories/controller.js
+++ src/apiServices/Categories/controller.js
@@ -1,7 +1,7 @@
 const dto = require('./dto');
 const action = require('./actions');
 const NodeCache = require("node-cache");
-const myCache = new NodeCache({ stdTTL: 360 });
+const myCache = new NodeCache({});
 
 async function put(req, res) {
   myCache.del('categories');
@@ -11,10 +11,12 @@ async function put(req, res) {
 
 async function get(req, res) {
   if (myCache.has(`categories`)) {
+    console.log('contenido cacheado')
     return res.send(myCache.get('categories'));
   } else {
     const category = await action.get(req.query.type);
-    myCache.set('categories', category);
+    myCache.set('categories', category, 300);
+    console.log('contenido no cacheado')
     return res.send(dto.single(category));
   }
 }
